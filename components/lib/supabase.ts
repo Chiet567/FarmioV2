9aec8a70fcfc2391b7d617739bdec6b2285eb873
Chiet567 <chiet21125@gmail.com>
2026-03-30 01:45:42 +0100

    Clear Supabase credentials in supabase.ts
    Removed Supabase URL and anon key for security.

--- components/lib/supabase.ts
+++ components/lib/supabase.ts
@@ -4,9 +4,9 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 import "react-native-url-polyfill/auto";
 
-const supabaseUrl = "https://weaycioahsuhucskpozg.supabase.co";
+const supabaseUrl = "";
 const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndlYXljaW9haHN1aHVjc2twb3pnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ1NTI3NzIsImV4cCI6MjA5MDEyODc3Mn0.LcRNXq4Ou-Lfl41C53CuvEDzL6L17PR0sUpBkItSMos";
+  "";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
